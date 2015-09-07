146fb3b5b891e39e3760c5a4bd47cd6338398c42
Martin Põldäär <martpol@greeny.cs.tlu.ee>
2015-09-07 15:43:08 +0300

    hellokitty2.php

--- helloworld2.php
+++ helloworld2.php
@@ -1,8 +1,36 @@
 <?php
 
        // user_form.php
+	   
+	   // jutumärkide vahele input elemendi NAME
+	   
 
+		//echo $_POST["email"]);
+		//echo $_POST["password"];
+		
+		$email_error = "";
+		$password_error = "";
+	    
+		//kontrolli ainult siis kui kasutaja vajutab "logi sisse" nuppu
+		if($_SERVER["REQUEST_METHOD"] == "POST") {
+			
+		//Kontrollime kasutaja eposti, et see ei oleks tühi
+		if(empty($_POST["email"])){
+		   $email_error = "Ei saa olla tühi";
+		}
+		// Kontrolli parooli
+		if(empty($_POST["password"])){
+			$password_error = "Ei saa olla tühi";
+		} else {
+			
+			// parool ei ole tühi, kontrollime pikkust
+			if(strlen($_POST["password"])) < 8 ){
+			
+			$password_error = "Peab olema vähemalt 8 sümbolit pikk" }
+		}
 
+		}
+		
 ?>
 
 <html>
@@ -11,10 +39,12 @@
 	</head>
 	<body>
 		<h2>Login</h2>
-		<input type="email" placeholder="E-post" >
-		<br><br>
-		<input type="password" placeholder="Salasõna" >
-		<input type ="submit" value="Logi sisse"
+		<form action="user_form.php" method="post">
+				<input name="email" type="email" placeholder="E-post" > <?php echo $email_error; ?>
+				<br><br>
+				<input name="password" type="password" placeholder="Salasõna" > <?php echo $password_error; ?>
+				<input type ="submit" value="Logi sisse">
+		</form>
 		<br><br>
 	
 	<h2>Create user</h2>
