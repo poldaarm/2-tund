414e95f1cb02c650fcbf480f29d0f49b034639a1
Martin Põldäär <martpol@greeny.cs.tlu.ee>
2015-09-07 13:49:27 +0300

    hellokitty.php

---
+++ helloworld.php
@@ -0,0 +1,22 @@
+<?php
+	$first_name = "Martin";
+	$last_name = "Põldäär";
+
+	//trükib välja
+	echo $first_name." ".$last_name;
+?>
+
+<br>
+
+<?php
+
+	$age = 22;
+	//if( loogikatehe ){ tõene } else { väär }
+	if($age < 18) {
+		echo "alaealine";
+	} else {
+		echo "täisealine";
+	}
+
+
+
