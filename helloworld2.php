f4c1a36a362f6d8660f847c335a596bdb57890b9
Martin Põldäär <martpol@greeny.cs.tlu.ee>
2015-09-07 14:58:02 +0300

    hellokitty.php

---
+++ helloworld2.php
@@ -0,0 +1,23 @@
+<?php
+
+       // user_form.php
+
+
+?>
+
+<html>
+	<head>
+		<title>User login page</title>
+	</head>
+	<body>
+		<h2>Login</h2>
+		<input type="email" placeholder="E-post" >
+		<br><br>
+		<input type="password" placeholder="Salasõna" >
+		<input type ="submit" value="Logi sisse"
+		<br><br>
+	
+	<h2>Create user</h2>
+		
+	</body>
+</html>
